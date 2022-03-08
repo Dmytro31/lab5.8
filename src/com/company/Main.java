96e67d1c592b8f4dea56e4d0c41dade275223404
Dmytro31 <99250554+Dmytro31@users.noreply.github.com>
2022-03-08 16:51:52 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ведіть число від 0 до 28800");
+        int s = scan.nextInt();
+        if(3600 > 0){
+        System.out.println("");
+        }
     }
 }
